00441701ffec52e69a96465c5409a99b5086a4a1
Fernando <riofriz@Fernandos-iMac.local>
2018-05-04 22:18:24 +0100

    testing

--- core/gamerchannel.js
+++ core/gamerchannel.js
@@ -91,7 +91,6 @@ module.exports = {
                             } else {
                                 if (typeof json_body['results'][key]['aliases'] !== 'undefined') {
                                     console.log(json_body['results'][key]['aliases']);
-                                    aliases += 'Mmmmh... maybe you could try this instead?\n ```';
                                     aliases += json_body['results'][key].name;
                                     // aliases = json_body['results'][key]['aliases'].replace(/\n/g, '').split(/\r/g);
                                     // if (aliases.findIndex(item => string.toLowerCase().trim() === item.toLowerCase().trim())) {
@@ -110,8 +109,10 @@ module.exports = {
                         message.channel.send(name);
                         message.channel.send(desc);
                     } else {
-                        message.channel.send(aliases+'```');
+                        message.channel.send('Mhhh.. maybe you could try this instead? \n\n ```'+aliases+'```');
                     }
+                } else {
+                    message.channel.send('mmmh.. are you actually sure you wanted to search '+string+'? Check for typos.. maybe.. ')
                 }
             }
         });
