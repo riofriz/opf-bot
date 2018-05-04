ac9c46e2e56d112640ebab874dafe6de2dafe23b
Fernando Campione <fernandocampione@Fernandos-iMac-2.local>
2018-05-04 16:30:22 +0100

    testing

--- core/gamerchannel.js
+++ core/gamerchannel.js
@@ -5,10 +5,7 @@ const Discord = require('discord.js');
 //let Parser = require('rss-parser');
 //let parser = new Parser();
 let string = '';
-var parser = require('xml2js');
 let https = require('https');
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-var xhr = new XMLHttpRequest();
 
 module.exports = {
     gamesearch: function(message, args) {
@@ -18,54 +15,37 @@ module.exports = {
             string += args[i]+' ';
         }
 
-        // let url = 'https://www.giantbomb.com';
-        // let path = '/api/search/'+qs.stringify('?api_key='+process.env.GIANTBOMB+'&query='+string);
-        //
-        // let options = {
-        //     host: url,
-        //     path: path,
-        //     "user-agent": "https://onepieceforum.net discord bot. For info contact comm.campione@gmail.com",
-        //     method: 'GET',
-        // };
-        //
-        // parser.on('error', function(err) { message.channel.send('Whops.. something must have gone wrong', err); });
-        // let data = '';
-        // https.request(options, function(res) {
-        //     if (res.statusCode >= 200) {
-        //         res.on('data', function(data_) { data += data_.toString(); });
-        //         res.on('end', function() {
-        //             console.log('data', data);
-        //             let rawName = data.game.name.replace('<![CDATA[ ', '');
-        //             let name = rawName.replace(' ]]>', '');
-        //             if (name === string) {
-        //                 messageToSend = name +' - '+ data.game.name;
-        //             } else {
-        //                 messageToSend = 'Please be more specific';
-        //             }
-        //
-        //             message.channel.send(messageToSend);
-        //
-        //             parser.parseString(data, function(err, result) {
-        //                 console.log('FINISHED', err, result);
-        //             });
-        //         });
-        //     } else {
-        //         console.log('error!! '+url+' '+res.statusCode+'\n'+res);
-        //     }
-        // });
-
         let url = 'https://www.giantbomb.com';
         let path = '/api/search/'+qs.stringify('?api_key='+process.env.GIANTBOMB+'&query='+string);
         let call = url+path;
-
-        XMLHttpRequest.open("GET", call, true);
-        XMLHttpRequest.onreadystatechange = function () {
-            if (XMLHttpRequest.readyState === 4 && XMLHttpRequest.status >= 200)
-            {
-                var doc = XMLHttpRequest.responseXML;
-                console.log(doc);
-            }
+        let options = {
+            host: url,
+            path: path,
+            headers: {'User-Agent': "https://onepieceforum.net discord bot. For info contact comm.campione@gmail.com"},
+            method: 'GET',
         };
-        XMLHttpRequest.send(null);
+
+
+        https.get(options, function (res) {
+            var json = '';
+            res.on('data', function (chunk) {
+                json += chunk;
+            });
+            res.on('end', function () {
+                if (res.statusCode >= 200) {
+                    try {
+                        var data = JSON.parse(json);
+                        // data is available here:
+                        console.log(data);
+                    } catch (e) {
+                        console.log('Error parsing JSON!');
+                    }
+                } else {
+                    console.log('Status:', res.statusCode);
+                }
+            });
+        }).on('error', function (err) {
+            console.log('Error:', err);
+        });
     }
 };
