753d5ac216d9a1cdf128543fc6da5625d5458040
Fernando <riofriz@Fernandos-iMac.local>
2018-05-04 19:34:40 +0100

    testing

--- core/gamerchannel.js
+++ core/gamerchannel.js
@@ -73,7 +73,7 @@ module.exports = {
                 if (json_body['number_of_page_results'] > 0) {
                     for (let key in json_body['results']) {
                         if (json_body['results'].hasOwnProperty(key)) {
-                            if (json_body['results'][key].aliases.findIndex(item => userCheck.toLowerCase().trim() === item.toLowerCase().trim())) {
+                            if (json_body['results'][key]['aliases'].findIndex(item => userCheck.toLowerCase().trim() === item.toLowerCase().trim())) {
                                 message.channel.send(json_body['results'][key].name);
                                 message.channel.send(striptags(json_body['results'][key].deck));
                             } else {
