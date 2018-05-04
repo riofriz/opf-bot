a0e2b2325bfe5f549084118c52f2bc475216f6ff
Fernando <riofriz@Fernandos-iMac.local>
2018-05-04 21:59:09 +0100

    testing

--- core/gamerchannel.js
+++ core/gamerchannel.js
@@ -89,14 +89,15 @@ module.exports = {
                                 name = json_body['results'][key].name;
                                 desc = striptags(json_body['results'][key].deck);
                             } else {
-                                if (json_body['results'][key]['aliases'] !== 'undefined') {
-                                    aliases = json_body['results'][key]['aliases'].replace(/\n/g, '').split(/\r/g);
-                                    if (aliases.findIndex(item => string.toLowerCase().trim() === item.toLowerCase().trim())) {
-                                        for (let j = 0; j !== aliases.length; j++) {
-                                            alternatives += aliases[j]+'\n';
-                                        }
-                                    }
-                                }
+                                console.log(json_body['results'][key]['aliases']);
+                                // if (json_body['results'][key]['aliases'] !== 'undefined') {
+                                //     aliases = json_body['results'][key]['aliases'].replace(/\n/g, '').split(/\r/g);
+                                //     if (aliases.findIndex(item => string.toLowerCase().trim() === item.toLowerCase().trim())) {
+                                //         for (let j = 0; j !== aliases.length; j++) {
+                                //             alternatives += aliases[j]+'\n';
+                                //         }
+                                //     }
+                                // }
                             }
                         }
                     }
