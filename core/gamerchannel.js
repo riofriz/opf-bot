3ad7003f16c9ef2b4c2b433ea987cce978bcfd4b
Fernando Campione <fernandocampione@Fernandos-iMac-2.local>
2018-05-04 16:50:28 +0100

    testing

--- core/gamerchannel.js
+++ core/gamerchannel.js
@@ -31,8 +31,8 @@ module.exports = {
             if (!error && response.statusCode === 200) {
                 let json_body = JSON.parse(body);
                 //console.log(json_body);
-                console.log(json_body['results']['aliases']);
-                console.log(striptags(json_body['results']['deck']));
+                console.log(json_body['results'][0]['aliases']);
+                console.log(striptags(json_body['results'][0]['deck']));
             } else {
                 console.log(error.message);
                 console.log(error);
