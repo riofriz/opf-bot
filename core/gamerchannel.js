94782ce6ffeba97198acec793324b76518093683
Fernando Campione <fernandocampione@Fernandos-iMac-2.local>
2018-05-04 17:48:09 +0100

    testing

--- core/gamerchannel.js
+++ core/gamerchannel.js
@@ -29,26 +29,27 @@ module.exports = {
 
         request(options, function (error, response, body) {
             if (!error && response.statusCode === 200) {
-                let json_body = JSON.parse(body);
-                let apiName;
-                if (json_body['number_of_page_results'] > 0) {
-                    if (json_body['results'][0]['aliases'] !== null) {
-                        apiName = json_body['results'][0]['aliases'].replace(/\n/g, '').split(/\r/g);
-                        let check = apiName[0].toLowerCase();
-                        let userCheck = string.toLowerCase();
-                        let gameDescription = striptags(json_body['results'][0]['deck']);
-                        console.log(apiName);
-                        console.log(check);
-                        console.log(userCheck);
-                        if (check.trim() === userCheck.trim()) {
-                            message.channel.send(check);
-                            message.channel.send(gameDescription);
-                        } else {
-                            message.channel.send('Please, be a bit more specific');
-                        }
+            let json_body = JSON.parse(body);
+            let apiName;
+            if (json_body['number_of_page_results'] > 0) {
+                if (json_body['results'][0]['aliases'] !== null) {
+                    apiName = json_body['results'][0]['aliases'].replace(/\n/g, '').split(/\r/g);
+                    let check = apiName[0].toLowerCase();
+                    let userCheck = string.toLowerCase();
+                    let gameDescription = striptags(json_body['results'][0]['deck']);
+                    console.log(apiName);
+                    console.log(check);
+                    console.log(userCheck);
+                    if (check.trim() === userCheck.trim()) {
+                        message.channel.send(check);
+                        message.channel.send(gameDescription);
                     } else {
-                        message.channel.send('Couldn\'t find anything for you.. sorry.. :( ');
+                        message.channel.send('Please, be a bit more specific');
                     }
+                } else {
+                    message.channel.send('Couldn\'t find anything for you.. sorry.. :( ');
+                }
+
             } else {
                 console.log(error.message);
                 console.log(error);
