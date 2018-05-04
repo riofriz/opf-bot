234210d21a8cd58aae04358e57bc3d83e7df50fa
Fernando <riofriz@Fernandos-iMac.local>
2018-05-04 22:01:57 +0100

    testing

--- core/gamerchannel.js
+++ core/gamerchannel.js
@@ -90,7 +90,7 @@ module.exports = {
                                 desc = striptags(json_body['results'][key].deck);
                             } else {
                                 console.log(json_body['results'][key]['aliases']);
-                                if (json_body['results'][key]['aliases'] !== undefined) {
+                                if (typeof json_body['results'][key]['aliases'] !== 'undefined') {
                                     aliases = json_body['results'][key]['aliases'].replace(/\n/g, '').split(/\r/g);
                                     if (aliases.findIndex(item => string.toLowerCase().trim() === item.toLowerCase().trim())) {
                                         for (let j = 0; j !== aliases.length; j++) {
