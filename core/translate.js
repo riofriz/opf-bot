b1d42f7d9758cd88f0a182e4fcb066200ae57a0f
Fernando <riofriz@Fernandos-iMac.local>
2018-05-05 03:40:50 +0100

    testing

--- core/translate.js
+++ core/translate.js
@@ -11,8 +11,14 @@ module.exports = {
         translate(string[1], {to: string[0]}).then(res => {
             console.log(res.text);
             //=> I speak English
-            console.log(res.from.language.iso);
+            console.log();
             //=> nl
+            let thumb = 'https://raw.githubusercontent.com/hjnilsson/country-flags/master/png1000px/'+res.from.language.iso+'.png';
+            let embed = new Discord.RichEmbed()
+                .setThumbnail(url=thumb)
+                .addField(res.from.language.iso, string[1])
+                .setColor(corevars.randomColor());
+            message.channel.send({embed: embed});
         }).catch(err => {
             console.error(err);
         });
