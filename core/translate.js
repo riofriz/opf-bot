49fb4377c23f364608aa0aa1662e736938518b38
Fernando <riofriz@Fernandos-iMac.local>
2018-05-05 03:42:49 +0100

    testing

--- core/translate.js
+++ core/translate.js
@@ -1,5 +1,6 @@
 const translate = require('google-translate-api');
 let corevars = require('./corevars');
+const Discord = require('discord.js');
 
 module.exports = {
     translateText: function(message, args) {
@@ -8,15 +9,15 @@ module.exports = {
             string += args[i] + ' ';
         }
         string = string.replace('undefined', '').split('>');
-        translate(string[1], {to: string[0]}).then(res => {
+        translate(string[1].trim(), {to: string[0].trim()}).then(res => {
             console.log(res.text);
             //=> I speak English
             console.log();
             //=> nl
-            let thumb = 'https://raw.githubusercontent.com/hjnilsson/country-flags/master/png1000px/'+res.from.language.iso+'.png';
+            let thumb = 'https://raw.githubusercontent.com/hjnilsson/country-flags/master/png1000px/'+res.from.language.iso.trim()+'.png';
             let embed = new Discord.RichEmbed()
                 .setThumbnail(url=thumb)
-                .addField(res.from.language.iso, string[1])
+                .addField(res.from.language.iso.trim(), string[1].trim())
                 .setColor(corevars.randomColor());
             message.channel.send({embed: embed});
         }).catch(err => {
