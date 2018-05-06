fe41097b0b1be3c14ea54a5f610bb97c19590993
Fernando <riofriz@Fernandos-iMac.local>
2018-05-07 00:03:32 +0100

    testing

--- core/forumapi.js
+++ core/forumapi.js
@@ -89,7 +89,7 @@ module.exports = {
                 console.log(err);
             } else {
                 obj = JSON.parse(data); //now it an object
-                obj.table.push(message.author.id = {'forumname':args[0]}); //add some data
+                obj.table.push(message.author.id[{'forumname':args[0]}]); //add some data
                 json = JSON.stringify(obj); //convert it back to json
                 fs.writeFile('logs/opfusers.json', json, 'utf8', callback); // write it back
                 message.channel.send('Ok, '+message.author+' i\'ll remember you are '+args[0]+' on the forum.');
