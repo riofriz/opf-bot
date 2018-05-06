b237868f9b7ca609b2a05af8348571d9aa26a0d9
Fernando <riofriz@Fernandos-iMac.local>
2018-05-06 01:35:48 +0100

    testing

--- core/fun.js
+++ core/fun.js
@@ -250,7 +250,7 @@ module.exports = {
         }
         if (typeof args[0] !== 'undefined') {
             mention = args[0];
-            if (mention.trim() === '<@408255473821679617>') {
+            if (mention.trim() === '<@408255473821679617>' && message.author.id !== '408255473821679617') {
                 dick = mention+' = 8==========================================D ';
             } else {
                 dick = mention+' = 8'+bodystr+'D '+ptff;
