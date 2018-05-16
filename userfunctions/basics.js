82a04b7f0460a2f1e191d17138535017d5c5842f
Fernando <riofriz@Fernandos-iMac.local>
2018-05-16 18:58:52 +0100

    test

--- userfunctions/basics.js
+++ userfunctions/basics.js
@@ -11,8 +11,7 @@ module.exports = {
     saveusers: function(message) {
         try {
             db.Users.update( { "id" : message.author.id }, { "id" : message.author.id },
-                    { "id" : message.author.id, "nick" : message.author.username, "botrank" : 0 },
-                    { upsert: true }, true
+                    { "id" : message.author.id, "nick" : message.author.username, "botrank" : 0 }, true
             );
         }
         catch (e){ console.log(e); }
