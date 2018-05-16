6fcfa8885e99d59b2370b8b105b1830c0109ef7e
Fernando <riofriz@Fernandos-iMac.local>
2018-05-16 19:29:11 +0100

    test

--- userfunctions/basics.js
+++ userfunctions/basics.js
@@ -10,7 +10,10 @@ let db = mongojs('mongodb://'+process.env.DBUSER+':'+process.env.DBPASSWORD+'@ds
 module.exports = {
     saveusers: function(message) {
         try {
-            let user = db.Users.find({} , {"id" : message.author.id } );
+            let user = db.Users.find(
+                {id : message.author.id }
+            );
+
             let commands;
             console.log(user);
             if (typeof user.triggeredCommands !== 'undefined') {
