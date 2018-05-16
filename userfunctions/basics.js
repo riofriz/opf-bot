dc4690348aa25b7aef8835a29e8b3370b37d961f
Fernando <riofriz@Fernandos-iMac.local>
2018-05-16 19:08:50 +0100

    test

--- userfunctions/basics.js
+++ userfunctions/basics.js
@@ -10,8 +10,11 @@ let db = mongojs('mongodb://'+process.env.DBUSER+':'+process.env.DBPASSWORD+'@ds
 module.exports = {
     saveusers: function(message) {
         try {
-            db.Users.update( { "id" : message.author.id }, { "id" : message.author.id },
-                    { "id" : message.author.id, "nick" : message.author.username, "botrank" : 0 }, {upsert: true}, function(err) {}
+            db.Users.update(
+                { "id" : message.author.id },
+                { "id" : message.author.id, "nick" : message.author.username, "botrank" : 0 },
+                {upsert: true},
+                function(err) {}
             );
         } catch (e){ console.log(e); }
     }
