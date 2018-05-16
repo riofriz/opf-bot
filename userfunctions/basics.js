b3032094a6c19b1b3320e0e97fdf8b40b899e738
Fernando <riofriz@Fernandos-iMac.local>
2018-05-16 23:04:42 +0100

    test

--- userfunctions/basics.js
+++ userfunctions/basics.js
@@ -132,7 +132,7 @@ module.exports = {
             let todayNoHours = today.setHours(0, 0, 0, 0);
             db.Users.findOne({"id": message.author.id}, function (err, doc) {
                 if (doc) {
-                    if (JSON.parse(JSON.stringify(doc)).hasOwnProperty('claims.latestClaim')) {
+                    if (JSON.parse(JSON.stringify(doc)).hasOwnProperty('claims')) {
                         latestClaim = doc.claims.latestClaim;
                         if (today.toDateString() !== latestClaim.toDateString()) {
                             console.log(today.toDateString());
