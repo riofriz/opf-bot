482960d52043d14491cd942f2f47d6281d13fd82
Fernando <riofriz@Fernandos-iMac.local>
2018-05-17 18:44:54 +0100

    test

--- rpg/corefunctions.js
+++ rpg/corefunctions.js
@@ -5,7 +5,7 @@ let https = require('https');
 let qs = require('qs');
 let request = require('request');
 let mongojs = require('mongojs');
-let jimp = require('jimp');
+let jimp = require('jimp2');
 let db = mongojs('mongodb://'+process.env.DBUSER+':'+process.env.DBPASSWORD+'@ds143362.mlab.com:43362/opmegabot', ['Users']);
 
 module.exports = {
