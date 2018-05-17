de60530030de050058d1fff4b1b940449c50ff63
Fernando Campione <fernandocampione@Fernandos-iMac-2.local>
2018-05-17 12:17:34 +0100

    test

--- userfunctions/commandsforberries.js
+++ userfunctions/commandsforberries.js
@@ -20,16 +20,38 @@ module.exports = {
           if (!message.guild.me.hasPermission('MANAGE_NICKNAMES')) {
               message.channel.send('I don\'t have permission to change your nickname!');
           } else {
+
+              let berries;
               try {
-                  message.member.setNickname(string).catch(err => {
-                      console.log(err);
-                      message.channel.send('You are too powerful for my to change your nick. Sorry Master.');
+                  db.Users.findOne({"id": message.author.id}, function (err, doc) {
+                      if (doc) {
+                          if (JSON.parse(JSON.stringify(doc)).hasOwnProperty('claims')) {
+                              berries = doc.claims.berries;
+                              if (berries >= 50) {
+                                  message.member.setNickname(string).catch(err => {
+                                      console.log(err);
+                                      message.channel.send('You are too powerful for me to change your nick. Sorry Master.');
+                                  });
+                                  db.Users.update(
+                                      {"id": message.author.id},
+                                      {$set: {"id": message.author.id, "claims": {"berries" : berries-50}}},
+                                      {upsert: true},
+                                      function (err) {}
+                                  );
+                                  message.channel.send('Your nick has been updated to *'+string+'* and your balance is now '+berries-50+'*B*');
+                              } else {
+                                  message.channel.send('Sorry, your balance is not enough to change nickname. Missing *B* :'+50-berries);
+                              }
+                          } else {
+                              message.channel.send('Your balance is still 0*B* claim some with o-claim');
+                          }
+                      } else {
+                          message.channel.send('Whops.. there might have been an error.');
+                      }
                   });
-              } catch (e){
-                  console.log(e);
               }
+              catch (e){ console.log(e); }
           }
-          // message.channel.send('<@'+message.author.id+'> Your username has been changed to: '+string);
       } else {
           message.channel.send('no argument passed');
       }
