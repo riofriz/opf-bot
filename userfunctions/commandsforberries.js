97cc4d288ae9cd603e454fb190b7809d4538166d
Fernando Campione <fernandocampione@Fernandos-iMac-2.local>
2018-05-17 12:55:30 +0100

    test

--- userfunctions/commandsforberries.js
+++ userfunctions/commandsforberries.js
@@ -31,13 +31,7 @@ module.exports = {
                               let balanceReturn = 50-berries;
                               let balanceLeft = berries-50;
                               if (berries >= 50) {
-                                  let bool = true;
-                                  message.member.setNickname(string).catch(err => {
-                                      console.log(err);
-                                      message.channel.send('You are too powerful for me to change your nick. Sorry Master.');
-                                      bool = false;
-                                  });
-                                  if (bool !== false) {
+                                  message.member.setNickname(string).try(success => {
                                       db.Users.update(
                                           {"id": message.author.id},
                                           {$set: {"id": message.author.id, "claims": {"berries": balanceLeft}}},
@@ -46,7 +40,11 @@ module.exports = {
                                           }
                                       );
                                       message.channel.send('Your nick has been updated to *' + string + '* and your balance is now ' + balanceLeft + '*B*');
-                                  }
+                                  }).catch(err => {
+                                      console.log(err);
+                                      message.channel.send('You are too powerful for me to change your nick. Sorry Master.');
+                                  });
+
                               } else {
                                   message.channel.send('Sorry, your balance is not enough to change nickname. Missing *B* :'+balanceReturn);
                               }
