355c5e7f65be496496e6045def239b9e92a797f1
Fernando <riofriz@Fernandos-iMac.local>
2018-05-17 20:37:41 +0100

    test

--- rpg/corefunctions.js
+++ rpg/corefunctions.js
@@ -5,26 +5,43 @@ let https = require('https');
 let qs = require('qs');
 let request = require('request');
 let mongojs = require('mongojs');
-let jimp = require('jimp');
+let Jimp = require('jimp');
 let db = mongojs('mongodb://'+process.env.DBUSER+':'+process.env.DBPASSWORD+'@ds143362.mlab.com:43362/opmegabot', ['Users']);
 
 module.exports = {
 
     wantedme: function(message) {
-        let thumb = new jimp(message.author.avatarURL, function (err, img) {
-            err ? console.log('logo err' + err) : console.log('logo created');
-            return img;
-        });
-        let font = jimp.FONT_SANS_16_BLACK;
+        // let thumb = new jimp(message.author.avatarURL, function (err, img) {
+        //     err ? console.log('logo err' + err) : console.log('logo created');
+        //     return img;
+        // });
+        // let font = jimp.FONT_SANS_16_BLACK;
+        //
+        // jimp.read('http://104.131.78.209/bot/rpg/userimages/emptywanted.jpg').then(function (lenna) {
+        //     lenna
+        //         .print(font, 20, 100, message.author.nickname)
+        //         .write(__dirname + '/userimages/'+message.author.id+'.jpg'); // save
+        //     message.channel.send('http://104.131.78.209/bot/rpg/userimages/'+message.author.id+'.jpg')
+        // }).catch(function (err) {
+        //     console.error(err);
+        // });
 
-        jimp.read('http://104.131.78.209/bot/rpg/userimages/emptywanted.jpg').then(function (lenna) {
-            lenna
-                .print(font, 20, 100, message.author.nickname)
-                .write(__dirname + '/userimages/'+message.author.id+'.jpg'); // save
-            message.channel.send('http://104.131.78.209/bot/rpg/userimages/'+message.author.id+'.jpg')
-        }).catch(function (err) {
+
+        Jimp.read("http://104.131.78.209/bot/rpg/userimages/emptywanted.jpg").then(function (delimg) {
+            Jimp.read(message.author.avatarURL).then(function(dimg) {
+                dimg.resize(114, 107);
+                delimg.composite(dimg, 82, 86);
+                Jimp.loadFont(Jimp.FONT_SANS_16_BLACK).then(function (font) {
+                    delimg.print(font, 265, 89, "Test string");
+                });
+                delimg.write(__dirname + '/userimages/'+message.author.id+'.jpg');
+                message.channel.send('http://104.131.78.209/bot/rpg/userimages/'+message.author.id+'.jpg');
+            });
+        }).catch(err => {
+            message.channel.send("An error occured!~\n```js\n" + err + "```");
             console.error(err);
-        });
+        })
+
 
         // console.log('this: '+thumb);
         // jimp.read('http://104.131.78.209/bot/rpg/userimages/emptywanted.jpg')
