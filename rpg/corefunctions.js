095130dfaa0b29071c601bc3d30486976eed24cc
Fernando <riofriz@Fernandos-iMac.local>
2018-05-17 18:39:20 +0100

    test

--- rpg/corefunctions.js
+++ rpg/corefunctions.js
@@ -15,19 +15,27 @@ module.exports = {
             err ? console.log('logo err' + err) : console.log('logo created');
             return img;
         });
-        console.log('this: '+thumb);
-        jimp.read('http://104.131.78.209/bot/rpg/userimages/emptywanted.jpg')
-            .then(function (image) {
-                image.composite(thumb, 25, 20)
-                    .crop( 0, 0, 0, 0 )
-                    .write(__dirname + '/userimages/'+message.author.id+'.jpg', function (err, success) {
-                        err ? console.log('this error is clone: '+err) : message.channel.send(__dirname+'/userimages/'+message.author.id+'jpg');
-                    });
-            }).then(function() {
-                message.channel.send(__dirname+'/userimages/'+message.author.id+'jpg');
-            }).catch(function (err){
-                console.log('this error is final catch: '+err);
-            });
+        
+        Jimp.read('http://104.131.78.209/bot/rpg/userimages/emptywanted.jpg').then(function (image) {
+            image.quality(60)
+                .write(message.author.id+'.jpg');
+        }).catch(function (err) {
+            console.error(err);
+        });
+
+        // console.log('this: '+thumb);
+        // jimp.read('http://104.131.78.209/bot/rpg/userimages/emptywanted.jpg')
+        //     .then(function (image) {
+        //         image.composite(thumb, 25, 20)
+        //             .crop( 0, 0, 0, 0 )
+        //             .write(__dirname + '/userimages/'+message.author.id+'.jpg', function (err, success) {
+        //                 err ? console.log('this error is clone: '+err) : message.channel.send(__dirname+'/userimages/'+message.author.id+'jpg');
+        //             });
+        //     }).then(function() {
+        //         message.channel.send(__dirname+'/userimages/'+message.author.id+'jpg');
+        //     }).catch(function (err){
+        //         console.log('this error is final catch: '+err);
+        //     });
     }
 
 };
