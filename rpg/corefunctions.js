6d4fe881f4f4262cf4a7cfbb67f8dfa61e3d6122
Fernando <riofriz@Fernandos-iMac.local>
2018-05-17 22:13:28 +0100

    test

--- rpg/corefunctions.js
+++ rpg/corefunctions.js
@@ -10,13 +10,25 @@ let db = mongojs('mongodb://'+process.env.DBUSER+':'+process.env.DBPASSWORD+'@ds
 
 module.exports = {
 
-    oprank: function(message) {
+    oprank: function(message, args, Client) {
 
         let berries;
-        let username = message.author.username;
         let commands;
         let rank;
+        let firstStrip;
+        let secondStrip;
+        let thirdStrip;
+        let howMuchLeft;
+        let user;
         try {
+            if (args[0]) {
+                firstStrip = args[0].trim().replace('<@', '');
+                secondStrip = firstStrip.replace('>', '');
+                thirdStrip = secondStrip.replace('!', '');
+                user = thirdStrip;
+            } else {
+                user = message.author.id;
+            }
             db.Users.findOne({"id": message.author.id}, function (err, doc) {
                 if (doc) {
                     if (JSON.parse(JSON.stringify(doc)).hasOwnProperty('claims')) {
@@ -24,24 +36,28 @@ module.exports = {
                         commands = doc.triggeredCommands;
                         rank = commands/250;
 
-                        Jimp.read(__dirname+"/userimages/emptywanted.png").then(function (delimg) {
-                            Jimp.read(message.author.avatarURL).then(function(dimg) {
-                                Jimp.loadFont(Jimp.FONT_SANS_64_BLACK).then(function (fonthuge) {
-                                    Jimp.loadFont(Jimp.FONT_SANS_32_BLACK).then(function (fontbig) {
-                                        dimg.resize(254, 247);
-                                        delimg.composite(dimg, 78, 130);
-                                        delimg.print(fontbig, 43, 440, ""+username.toUpperCase()+"");
-                                        delimg.print(fontbig, 78, 500, ""+berries+"");
-                                        delimg.print(fonthuge, 285, 440, ""+Math.floor(rank)+"");
-                                        delimg.write(__dirname + '/userimages/'+message.author.id+'.jpg');
-                                        message.channel.send({file: __dirname + '/userimages/'+message.author.id+'.jpg'});
+                        Client.fetchUser(user).then(myUser => {
+                            let username = myUser.username;
+                            Jimp.read(__dirname+"/userimages/emptywanted.png").then(function (delimg) {
+                                Jimp.read(myUser.avatarURL).then(function(dimg) {
+                                    Jimp.loadFont(Jimp.FONT_SANS_64_BLACK).then(function (fonthuge) {
+                                        Jimp.loadFont(Jimp.FONT_SANS_32_BLACK).then(function (fontbig) {
+                                            dimg.resize(254, 247);
+                                            delimg.composite(dimg, 78, 130);
+                                            delimg.print(fontbig, 43, 440, ""+username.toUpperCase()+"");
+                                            delimg.print(fontbig, 78, 500, ""+berries+"");
+                                            delimg.print(fonthuge, 285, 440, ""+Math.floor(rank)+"");
+                                            delimg.write(__dirname + '/userimages/'+user+'.jpg');
+                                            message.channel.send({file: __dirname + '/userimages/'+user+'.jpg'});
+                                        });
                                     });
                                 });
-                            });
-                        }).catch(err => {
-                            message.channel.send("An error occured!~\n```js\n" + err + "```");
-                            console.error(err);
-                        })
+                            }).catch(err => {
+                                message.channel.send("Whops.. that didn't work for some weird reason.");
+                                console.error(err);
+                            })
+                        });
+
                     } else {
                         message.channel.send('Your balance is still 0*B*, try claiming some *B* with o-claim');
                     }
