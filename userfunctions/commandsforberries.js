554998c119e71ea1315f9f0fbdaff09464461f9c
Fernando Campione <fernandocampione@Fernandos-iMac-2.local>
2018-05-17 10:55:37 +0100

    test

--- userfunctions/commandsforberries.js
+++ userfunctions/commandsforberries.js
@@ -18,7 +18,7 @@ module.exports = {
           for (let i = 0; i !== args.length; i++) {
               string += args[i] + ' ';
           }
-          message.guild.members.get(message.author.id).setNickname(string);
+          message.guild.members.get(message.author).setNickname(string);
           if (message.guild.members.get(client.user.id).hasPermission("MANAGE_NICKNAMES") && message.guild.members.get(client.user.id).hasPermission("CHANGE_NICKNAME")) {
               message.guild.members.get(message.author.id).setNickname(string);
               message.channel.send('<@'+message.author.id+'> Your username has been changed to: '+string);
