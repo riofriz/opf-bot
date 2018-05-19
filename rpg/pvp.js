f2e3a2d394434b9580c5c4666f4c260629785074
Fernando <riofriz@Fernandos-iMac.local>
2018-05-19 02:08:14 +0100

    test

--- rpg/pvp.js
+++ rpg/pvp.js
@@ -62,16 +62,51 @@ module.exports = {
                                 message.channel.send('You can\'t afford that much.. Unless you are cheating <:pfft:445023527888748544>');
                             } else {
                                 //BATTLE COMMAND IN HERE.
-                                let diceroll = Math.random() * 9;
+                                let diceroll = Math.random() * 12;
                                 diceroll = Math.floor(diceroll);
                                 let embed;
+                                let mobs = [
+                                    'a *pirate*',
+                                    'a *fishman*',
+                                    'a *bounty hunter*',
+                                    'a *sea monster*',
+                                    'a *marine*',
+                                    '*Foxy*'
+                                ];
+                                let randomNumber = Math.floor(Math.random()*mobs.length);
+                                let image;
+                                if (randomNumber === 0) {image = 'pirate.jpg'}
+                                else if (randomNumber === 1) {image = 'fishman.png'}
+                                else if (randomNumber === 3) {image = 'bountyhunter.png'}
+                                else if (randomNumber === 4) {image = 'seamonster.png'}
+                                else if (randomNumber === 5) {image = 'marine.png'}
+                                else if (randomNumber === 6) {image = 'foxy.jpg'}
+
                                 if (diceroll > 6) {
+                                    let winnings;
+                                    if (diceroll === 1){winnings = (16.666 / 100) * args[0];}
+                                    else if (diceroll === 2){winnings = (33.332 / 100) * args[0];}
+                                    else if (diceroll === 3){winnings = (49.998 / 100) * args[0];}
+                                    else if (diceroll === 4){winnings = (66.664 / 100) * args[0];}
+                                    else if (diceroll === 5){winnings = (83.33 / 100) * args[0];}
+                                    else if (diceroll === 6){winnings = args[0];}
                                     embed = new Discord.RichEmbed()
                                         .setThumbnail(url='http://104.131.78.209/bot/rpg/dices/' + diceroll + '.png')
+                                        .setTitle('You are now attacking '+mobs[randomNumber]+'.')
+                                        .setImage('http://104.131.78.209/bot/rpg/mobs/'+image)
                                         .setColor(corevars.randomColor());
                                 } else {
+                                    let winnings;
+                                    if (diceroll === 1){winnings = (16.666 / 100) * args[0];}
+                                    else if (diceroll === 2){winnings = (33.332 / 100) * args[0];}
+                                    else if (diceroll === 3){winnings = (49.998 / 100) * args[0];}
+                                    else if (diceroll === 4){winnings = (66.664 / 100) * args[0];}
+                                    else if (diceroll === 5){winnings = (83.33 / 100) * args[0];}
+                                    else if (diceroll === 6){winnings = args[0];}
                                     embed = new Discord.RichEmbed()
                                         .setThumbnail(url='http://104.131.78.209/bot/rpg/dices/' + diceroll + '.png')
+                                        .setImage('http://104.131.78.209/bot/rpg/mobs/'+image)
+                                        .setTitle('You are now attacking '+mobs[randomNumber]+'.')
                                         .setColor(corevars.randomColor());
                                 }
                                 message.channel.send({embed: embed});
