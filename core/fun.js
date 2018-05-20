6a9a7dff5ebd9106dc01d4ce61b8f0f1bdc800f9
Fernando <riofriz@Fernandos-iMac.local>
2018-05-20 22:00:16 +0100

    test

--- core/fun.js
+++ core/fun.js
@@ -404,7 +404,7 @@ module.exports = {
             url: 'https://api.tenor.com/v1/random',
             method: 'GET',
             headers: headers,
-            qs: {'key': process.env.TENOR, 'q': 'mapache'}
+            qs: {'key': process.env.TENOR, 'q': 'racoon'}
         };
 
         request(options, function (error, response, body) {
