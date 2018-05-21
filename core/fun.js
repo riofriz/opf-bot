532ef1d61b20b08d72ed1218986a10d4356cdefb
Fernando <riofriz@Fernandos-iMac.local>
2018-05-21 18:47:54 +0100

    test

--- core/fun.js
+++ core/fun.js
@@ -461,7 +461,7 @@ module.exports = {
                         message.channel.send(json_body['videos'][randomNumber]['video']['url'] + ' <:eggplanthand:446252709910413312> <:eggplanthand:446252709910413312>');
                     } else {
                         message.channel.send('Whops.. sorry little pervert, couldn\'t find what you were looking for.. <:eggplanthand:446252709910413312> <:eggplanthand:446252709910413312>');
-                        console.log(json_body);
+                        console.log(json_body['videos'][0]['video']);
                     }
                 } else {
                     console.log(error.message);
