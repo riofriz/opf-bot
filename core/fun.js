cb53f67d7ce73506cc6e023e81fb9c3133050681
Fernando Campione <fernandocampione@Fernandos-iMac-2.local>
2018-05-29 10:19:01 +0100

    wuami

--- core/fun.js
+++ core/fun.js
@@ -1135,7 +1135,7 @@ module.exports = {
 
         let headers = {
             'User-Agent': 'https://onepieceforum.net discord bot. For info contact comm.campione@gmail.com',
-            'Content-Type': 'application/json'
+            'Content-Type': 'text/plain'
         };
 
         let options = {
