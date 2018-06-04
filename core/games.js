10467920aade5d05ffed36a4a7f6ede6cc24431f
Fernando <riofriz@Fernandos-iMac.local>
2018-06-04 18:32:21 +0100

    no pizza

--- core/games.js
+++ core/games.js
@@ -6,13 +6,13 @@ module.exports = {
         if (args[0]) {
             let interval;
             if (args[0].trim() === 'on') {
-                interval = setInterval(function () {
-                    // use the message's channel (TextChannel) to send a new message
-                    message.channel.send("popquiztest")
-                        .catch(console.error); // add error handling here
-                }, 5000);
+                let t = ()=>
+                    setInterval(()=>{
+                        message.channel.send("popquiztest")
+                    },5000);
             } else if (args[0].trim() === 'off') {
-                clearInterval(interval)
+                interval = t();
+                clearInterval(interval);
             }
         }
     }
