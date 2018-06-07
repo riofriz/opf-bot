749a36a1ce64546973b66fca5d095d73410b7405
Fernando Campione <fernandocampione@Fernandos-iMac-2.local>
2018-06-07 15:27:07 +0100

    quiz

--- core/games.js
+++ core/games.js
@@ -1,4 +1,6 @@
 const Discord = require('discord.js');
+const Trivia = require('trivia-api');
+const trivia = new Trivia({ encoding: 'url3986' });
 
 module.exports = {
 
@@ -8,9 +10,19 @@ module.exports = {
             if (args[0].trim() === 'on') {
                 interval = setInterval(function () {
                     // use the message's channel (TextChannel) to send a new message
+                    let options = {
+                        amount: 1,
+                        difficulty: 'medium'
+                    };
+                    trivia.getQuestions(options)
+                        .then(questions => {
+                            console.log(questions);
+                        })
+                        .catch(console.error);
+
                     message.channel.send("popquiztest")
                         .catch(console.error); // add error handling here
-                }, 5000);
+                }, 900000);
             } else if (args[0].trim() === 'off') {
                 return clearInterval(interval);
             }
