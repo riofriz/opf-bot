5e03522052a06f531317ad589ce582ef53c3b576
Fernando Campione <fernandocampione@Fernandos-iMac-2.local>
2018-08-28 13:02:48 +0100

    alexa play

--- core/fun.js
+++ core/fun.js
@@ -85,7 +85,7 @@ module.exports = {
 
     yt: function(message) {
         let query;
-        query = query.replace('alexa play', '');
+        query = message.replace('alexa play', '');
         if(query !== '') {
             query = query.trim();
         } else {
